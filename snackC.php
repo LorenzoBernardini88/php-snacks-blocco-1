703283556f33671f4b3ad9e33c109a3c2b3e6acd
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-02-15 15:45:04 +0100

    prova snackC

---
+++ snackC.php
@@ -0,0 +1,30 @@
+<!-- Snack C
+Creare un array con 15 numeri casuali, 
+tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta. -->
+
+<!-- SVOLGIMENTO -->
+<?php
+$numArray=[];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SnackC</title>
+</head>
+<body>
+    <?php
+    
+    echo 'ciao';
+    while (count($numArray) == 15) {
+        $num = rand(0,100);
+        if(!in_array($num,$numArray)){
+            $numArray[] = $num;
+        }
+        echo $numArray;
+    }
+    ?>
+</body>
+</html>
